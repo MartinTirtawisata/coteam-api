4afc3076a63d20a69ac008fe3d5639feb9880277
Martin Tirtawsata <martin.tirtawisata@hotmail.com>
2018-11-20 15:49:10 -0800

    mongo

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/react-api';
+// exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/react-api';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://martin:martin3@ds151943.mlab.com:51943/coteam-api';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/react-api';
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://hidden-shelf-59966.herokuapp.com/';
 exports.JWT_SECRET = process.env.JWT_SECRET;
