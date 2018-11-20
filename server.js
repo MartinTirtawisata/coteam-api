e98cde8ebf7ee02b574edf4149b37d7b910b687f
Martin Tirtawsata <martin.tirtawisata@hotmail.com>
2018-11-20 09:26:08 -0800

    fixed cors

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+app.use(cors());
+
 const passport = require('passport');
 
 const mongoose = require('mongoose');
@@ -12,9 +14,7 @@ const usersRouter = require('./users/usersRouter')
 
 const {router: authRouter, localStrategy, jwtStrategy} = require('./auth')
 
-app.use(
-    cors()
-);
+
 // app.use(function (req, res, next) {
 //     res.header('Access-Control-Allow-Origin', '*');
 //     res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
