939c66b7ca6f3f656de917250c3746368ce529f3
Martin Tirtawsata <martin.tirtawisata@hotmail.com>
2018-11-20 11:47:55 -0800

    fixed server

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const cors = require('cors');
 const app = express();
 const {DATABASE_URL, CLIENT_ORIGIN} = require('./config');
 app.use(cors({
-    origin: CLIENT_ORIGIN
+    origin: CLIENT_ORIGIN    
 }));
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-    if (req.method === 'OPTIONS') {
-      return res.send(204);
-    }
-    next();
-});
+// app.use(function (req, res, next) {
+    
+//     res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+//     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+//     if (req.method === 'OPTIONS') {
+//       return res.send(204);
+//     }
+//     next();
+// });
 
 const passport = require('passport');
 
